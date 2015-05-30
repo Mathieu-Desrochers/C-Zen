8be1e6b675cbb09263285816da2be4827edab34d
Mathieu Desrochers <mathieu@desrochers>
2015-05-30 15:03:22 -0400

    That should be it for tables

--- sources/core/main/main.c
+++ sources/core/main/main.c
@@ -1,32 +1,31 @@
 #include <stdlib.h>
 
+#include "../../core/tables/order_item_row.h"
+#include "../../core/tables/order_items_table.h"
 #include "../../infrastructure/dbg/dbg.h"
-#include "../../core/services/new_order_request.h"
-#include "../../core/services/new_order_request_order_item.h"
+#include "../../infrastructure/sql/sql.h"
 
 int main()
 {
-  new_order_request_t *new_order_request = new_order_request_malloc("Alice", 100);
-  check(new_order_request != NULL, "new_order_request: NULL");
+  order_item_row_t **order_item_rows = NULL;
+  int count = 0;
 
-  new_order_request->order_items = malloc(sizeof(new_order_request_order_item_t *) * 2);
-  check_mem(new_order_request->order_items);
+  sqlite3 *sql_connection;
+  int sql_open_connection_result = sql_open_connection("/var/c-zen/c-zen.db", &sql_connection);
+  check(sql_open_connection_result == 0, "sql_open_connection_result: %d",
+    sql_open_connection_result);
 
-  new_order_request->order_items_count = 2;
+  int result = order_items_table_select_all(sql_connection, &order_item_rows, &count);
+  check(result == 0, "X");
 
-  new_order_request->order_items[0] = new_order_request_order_item_malloc("Skateboard", 2);
-  check(new_order_request->order_items[0] != NULL, "new_order_request->order_items[0]: NULL");
+  order_item_rows_free(order_item_rows, count);
 
-  new_order_request->order_items[1] = new_order_request_order_item_malloc("Pirate sword", 5);
-  check(new_order_request->order_items[1] != NULL, "new_order_request->order_items[1]: NULL");
-
-  new_order_request_free(new_order_request);
+  sql_close_connection(sql_connection);
 
   return 0;
 
 error:
 
-  if (new_order_request != NULL) { new_order_request_free(new_order_request); }
-
-  return -1;
+  if (order_item_rows != NULL) { order_item_rows_free(order_item_rows, count); }
+  if (sql_connection != NULL) { sql_close_connection(sql_connection); }
 }
