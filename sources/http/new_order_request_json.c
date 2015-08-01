9f579bf614f9e982fdbacfea5525313a8a418b24
Mathieu Desrochers <mathieu@desrochers>
2015-08-01 14:07:53 -0400

    Whole json parse / format functionality

--- sources/http/new_order_request_json.c
+++ sources/http/new_order_request_json.c
@@ -16,11 +16,10 @@ int new_order_request_json_parse(json_t *json, new_order_request_t **new_order_r
   char *customer_name = NULL;
   int *total = NULL;
 
-  json_t *order_items_json = NULL;
-  int order_items_json_count = 0;
-  json_t *order_item_json = NULL;
   new_order_request_order_item_t *new_order_request_order_item = NULL;
+  new_order_request_order_item_t **new_order_request_order_items = NULL;
   int allocated_new_order_request_order_items_count = 0;
+  int used_new_order_request_order_items_count = 0;
 
   check(json != NULL, "json: NULL");
   check(new_order_request != NULL, "new_order_request: NULL");
@@ -33,11 +32,8 @@ int new_order_request_json_parse(json_t *json, new_order_request_t **new_order_r
   check(json_object_get_total_result == 0, "json_object_get_total_result: %d",
     json_object_get_total_result);
 
-  new_order_request_return = new_order_request_malloc(
-    customer_name,
-    total);
-
-  check(new_order_request_return != NULL, "new_order_request_return: NULL");
+  json_t *order_items_json = NULL;
+  int order_items_json_count = 0;
 
   int json_object_get_order_items_result = json_object_get_array(json, "order-items", &order_items_json, &order_items_json_count);
   check(json_object_get_order_items_result == 0, "json_object_get_order_items_result: %d",
@@ -47,6 +43,8 @@ int new_order_request_json_parse(json_t *json, new_order_request_t **new_order_r
   {
     for (int i = 0; i < order_items_json_count; i++)
     {
+      json_t *order_item_json = NULL;
+
       int json_array_get_order_item_result = json_array_get_object(order_items_json, i, &order_item_json);
       check(json_array_get_order_item_result == 0, "json_array_get_order_item_result: %d",
         json_array_get_order_item_result);
@@ -62,9 +60,9 @@ int new_order_request_json_parse(json_t *json, new_order_request_t **new_order_r
       }
 
       int array_add_pointer_result = array_add_pointer(
-        (void ***)&(new_order_request_return->order_items),
+        (void ***)&new_order_request_order_items,
         &allocated_new_order_request_order_items_count,
-        &(new_order_request_return->order_items_count),
+        &used_new_order_request_order_items_count,
         new_order_request_order_item);
 
       check(array_add_pointer_result == 0, "array_add_pointer_result: %d",
@@ -73,6 +71,15 @@ int new_order_request_json_parse(json_t *json, new_order_request_t **new_order_r
       new_order_request_order_item = NULL;
     }
   }
+
+  new_order_request_return = new_order_request_malloc(
+    customer_name,
+    total);
+
+  check(new_order_request_return != NULL, "new_order_request_return: NULL");
+
+  new_order_request_return->order_items = new_order_request_order_items;
+  new_order_request_return->order_items_count = used_new_order_request_order_items_count;
 
   free(customer_name);
   free(total);
@@ -87,6 +94,11 @@ error:
   if (customer_name != NULL) { free(customer_name); }
   if (total != NULL) { free(total); }
   if (new_order_request_order_item != NULL) { new_order_request_order_item_free(new_order_request_order_item); }
+
+  if (new_order_request_order_items != NULL)
+  {
+    new_order_request_order_items_free(new_order_request_order_items, used_new_order_request_order_items_count);
+  }
 
   return -1;
 }
